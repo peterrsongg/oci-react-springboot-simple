8f943faf7b6bfdc0d3d03b71f0476c7ff6ad920f
Peter Song <petersong98@gmail.com>
2022-04-01 15:13:51 -0700

    comment out cors for testing

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/MyTodoListApplication.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/MyTodoListApplication.java
@@ -2,6 +2,11 @@ package com.springboot.MyTodoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
 public class MyTodoListApplication {
@@ -9,5 +14,4 @@ public class MyTodoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyTodoListApplication.class, args);
 	}
-
 }
